8172131c7718501c80c283ab9ff71c55da1034c2
Nitin chaudhary <nitinchaudhary5967@gmail.com>
2026-01-21 12:49:21 +0530

    [Nitin Chaudhary]

---
+++ basic-programs/02-Java-Primitive-Data-Types/FeesDiscount.java
@@ -0,0 +1,9 @@
+class FeesDiscount{
+	public static void main(String args[]){
+		int Fees = 125000;
+		int discountpercent = 10;
+		double discountamount = Fees/discountpercent;
+		double discountedfees = Fees-discountamount;
+		System.out.println("The discount amount is INR "+discountamount+" and final discounted fee is INR "+discountedfees);
+		}
+	}
---
+++ basic-programs/02-Java-Primitive-Data-Types/HarryAge.java
@@ -0,0 +1,8 @@
+class HarryAge{
+     public static void main(String args[]){
+	    int birthyear = 2000;
+		int currentyear = 2024;
+		int Age = currentyear-birthyear;
+		System.out.println("Harry's age in "+currentyear+" is "+Age);
+		}
+	 }

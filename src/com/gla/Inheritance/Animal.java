03a1a85a9d1da542e98decaa22f05505f25ca17d
Nitin chaudhary <nitinchaudhary5967@gmail.com>
2026-02-17 12:12:41 +0530

    [nitin]

--- src/com/gla/Inheritance/Animal.java
+++
@@ -1,8 +0,0 @@
-package com.gla.Inheritance;
-
-public class Animal {
-    public void walk;
-    walk();
-    System.out.println("walk on four legs")
-
-}
--- src/com/gla/Inheritance/Dog.java
+++
@@ -1,7 +0,0 @@
-package com.gla.Inheritance;
-
-public class Dog {
-    public void Barks;
-    Barks();
-    System.out.println("Barks")
-}

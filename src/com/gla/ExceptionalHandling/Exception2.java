316b6f1d41ae59ba75c419e052a69203a498dac0
Nitin chaudhary <nitinchaudhary5967@gmail.com>
2026-03-17 11:48:32 +0530

    [Nitin]

---
+++ src/com/gla/ExceptionalHandling/Exception2.java
@@ -0,0 +1,17 @@
+package com.gla.ExceptionalHandling;
+
+import java.sql.SQLOutput;
+
+public class Exception2 {
+    public static void divide(int a, int b) throws ArithmeticException{
+        int c =a/b;
+        System.out.println();
+    }
+    public static void main(String[]args){
+        try{
+            divide(5,0);
+        }catch (ArithmeticException e){
+            System.out.println(e.getMessage()); 
+        }
+    }
+}

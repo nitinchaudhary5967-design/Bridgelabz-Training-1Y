dd56fb6c4a87c5cbf2119b681be8b501e9af00f1
Nitin chaudhary <nitinchaudhary5967@gmail.com>
2026-04-03 22:42:36 +0530

    [Nitin]

--- src/com/gla/type_casting/Explicit.java
+++
@@ -1,9 +0,0 @@
-package com.gla.type_casting;
-
-public class Explicit {
-    static void main(String[]args){
-        double d = 10.45;
-        int x = (int) d;
-        System.out.println(x);
-    }
-}
